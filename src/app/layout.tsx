4e94baf7697a511aecb3a1e9764942ddce4aa6ef
Sadra Khosravi <sadrakh@outlook.com>
2024-11-02 14:50:14 -0700

    feat: framework change, no login required, moving to next.js

---
+++ src/app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+import { Onest } from 'next/font/google'
+ 
+// If loading a variable font, you don't need to specify the font weight
+const onest = Onest({ subsets: ['latin'],
+  adjustFontFallback: true,
+  preload: true,
+  display: 'swap',
+ })
+
+export const metadata: Metadata = {
+  title: "Spiral Flow",
+  description: "",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${onest.className} antialiased dark`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
