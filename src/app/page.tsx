306037562f1c08667e1cdfc49a4dbd58044159dc
Sadra Khosravi <sadrakh@outlook.com>
2024-11-21 17:11:04 -0800

    feat(components): update component styling to match the theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,12 +6,16 @@ import { PageControls } from '@/components/controls/page-controls';
 
 export default function Home() {
   return (
-    <div className="min-w-screen container mx-auto flex h-full min-h-screen flex-col gap-4 p-2 px-4 pb-0">
+    <div className="min-w-screen mx-auto flex h-full min-h-screen flex-col gap-4 p-2 px-4 pb-0">
       <Header />
-      <main className="flex h-full w-full items-center gap-4">
-        <ParameterControls />
+      <main className="flex h-full w-full items-center gap-8">
+        <div className="h-full pt-2">
+          <ParameterControls />
+        </div>
         <Preview />
-        <PageControls />
+        <div className="h-full pt-2">
+          <PageControls />
+        </div>
       </main>
     </div>
   );
